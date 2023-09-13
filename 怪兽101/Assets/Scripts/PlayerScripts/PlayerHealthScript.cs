271da9f61e61bfd9bab6239c834901c8797e464d
BransonAng98 <bransonang@habby.com>
2023-09-13 16:37:32 +0800

    UI update
    Player can now take damage from spacebar key which is reflected on the UI. additionally the healthbar has a shake feature that can be adjusted.

---
+++ 怪兽101/Assets/Scripts/PlayerScripts/PlayerHealthScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthScript : MonoBehaviour
+{
+    public PlayerStatScriptableObject playerSO; 
+    public Slider healthSlider;
+    public Image healthFill;
+    public float lerpSpeed = 2f;
+
+    private ShakeScript shakeMe;
+    private float currentHealth;
+
+    private void Start()
+    {
+        shakeMe = healthSlider.gameObject.GetComponent<ShakeScript>();
+        playerSO.health = 100;
+        currentHealth = playerSO.health; // Set initial health to full
+        UpdateHealthBar();
+    }
+
+    private void Update()
+    {
+        if (currentHealth != playerSO.health)
+        {
+            // Lerp towards the target health value
+            currentHealth = Mathf.Lerp(currentHealth, playerSO.health, Time.deltaTime * lerpSpeed);
+            UpdateHealthBar();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage();
+        }
+    }
+
+    public void TakeDamage()
+    {
+        shakeMe.StartShake();
+        playerSO.health -= 5; // Adjust the damage amount as needed
+        Debug.Log("ouch" + playerSO.health);
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthSlider.value = currentHealth; // Update the slider's value
+        healthFill.fillAmount = currentHealth; // Update the fill amount of the health bar
+    }
+
+
+}
---
+++ 怪兽101/Assets/Scripts/ShakeScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeScript : MonoBehaviour
+{
+    public float shakeDuration = 0.2f;
+    public float shakeStrength = 10f;
+
+    private Vector3 originalPosition;
+    private Transform targetTransform;
+
+    private void Start()
+    {
+        targetTransform = transform;
+        originalPosition = targetTransform.localPosition;
+    }
+
+    public void StartShake()
+    {
+        StartCoroutine(ShakeCoroutine());
+    }
+
+    private IEnumerator ShakeCoroutine()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < shakeDuration)
+        {
+            Vector3 randomOffset = Random.insideUnitSphere * shakeStrength;
+            targetTransform.localPosition = originalPosition + randomOffset;
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Reset the position to the original after the shake is finished
+        targetTransform.localPosition = originalPosition;
+    }
+
+}
