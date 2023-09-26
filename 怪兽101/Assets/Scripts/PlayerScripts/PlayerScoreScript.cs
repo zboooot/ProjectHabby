53fb0e4c618fb538136d1b24cf20d27c85be1f57
BransonAng98 <bransonang@habby.com>
2023-09-26 14:32:46 +0800

    AirStrikeUpdate
    planes now randomize between left n right of the screen

--- 怪兽101/Assets/Scripts/PlayerScripts/PlayerScoreScript.cs
+++ 怪兽101/Assets/Scripts/PlayerScripts/PlayerScoreScript.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScoreScript : MonoBehaviour
 {
     public int entitiesDestroyedCount = 0;
     public GameObject enemyPlane;
     public Animator anim;
-    public GameObject spawnPoint;
+    public Transform[] spawnPoints;
 
     private void Start()
     {
         anim = GameObject.Find("MilitaryAbilityWarning").GetComponent<Animator>();
+
+        // Check if we have at least one spawn point
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("No spawn points assigned!");
+            return;
+        }
     }
 
 
@@ -27,10 +35,6 @@ public class PlayerScoreScript : MonoBehaviour
         }
 
     }
-    public void OnAnimationEnd()
-    {
-        //ActivateSkill(); 
-    }
 
     public void ActivateSkill()
     {
@@ -38,16 +42,22 @@ public class PlayerScoreScript : MonoBehaviour
         {
             anim.SetBool("Close", true);
 
-            // Set the Z position to 0 to avoid it being hidden
-            Vector3 spawnPosition = new Vector3(spawnPoint.transform.position.x, spawnPoint.transform.position.y, 0f);
+            // Randomly choose a spawn point
+            Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
 
             // Instantiate the GameObject at the desired position
-            GameObject fighterJet = Instantiate(enemyPlane, spawnPosition, transform.rotation);
+            SpawnObject(randomSpawnPoint);
 
             Debug.Log("Ultimate skill activated!");
             entitiesDestroyedCount = 0;
 
         }
+    }
+
+    public void SpawnObject(Transform spawnPoint)
+    {
+        GameObject fighterJet = Instantiate(enemyPlane, spawnPoint.position, spawnPoint.rotation);
+        fighterJet.transform.position = new Vector3(fighterJet.transform.position.x, fighterJet.transform.position.y, 0f);
     }
 
 }
